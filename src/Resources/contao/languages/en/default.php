cec5713dc86ee49cee6d30e525dc1fd273378b07
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-11-08 12:51:51 -0500

    Create default.php

---
+++ src/Resources/contao/languages/en/default.php
@@ -0,0 +1,18 @@
+<?php
+
+  /**
+   * @copyright  Bright Cloud Studio
+   * @author     Bright Cloud Studio
+   * @package    Contao CE Grouping
+   * @license    LGPL-3.0+
+   * @see	       https://github.com/bright-cloud-studio/contao-ce-grouping
+   */
+
+  $GLOBALS['TL_LANG']['CTE']['grouping'] = 'Grouping Wrapper';
+  $GLOBALS['TL_LANG']['CTE']['grouping_start'][0] = 'Grouping Start';
+  $GLOBALS['TL_LANG']['CTE']['grouping_start'][1] = 'Start Grouping wrapper';
+  
+  $GLOBALS['TL_LANG']['CTE']['grouping_stop'][0] = 'Grouping Stop';
+  $GLOBALS['TL_LANG']['CTE']['grouping_stop'][1] = 'Stop Grouping wrapper';
+
+?>
