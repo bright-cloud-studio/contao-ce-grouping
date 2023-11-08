7a3e043db5caef5f486427f07413dfbfd21546ec
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-11-08 13:16:15 -0500

    Create tl_content.php

---
+++ src/Resources/contao/dca/tl_content.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @copyright  Bright Cloud Studio
+ * @author     Bright Cloud Studio
+ * @package    Contao CE Grouping
+ * @license    LGPL-3.0+
+ * @see	       https://github.com/bright-cloud-studio/contao-ce-grouping
+ */
+
+$GLOBALS['TL_DCA']['tl_content']['palettes']['grouping_start'] = '{type_legend},type,headline;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},cssID;{invisible_legend:hide},invisible,start,stop';
+$GLOBALS['TL_DCA']['tl_content']['palettes']['grouping_stop'] = '{type_legend},type;{template_legend:hide},customTpl;{protected_legend:hide},protected;{invisible_legend:hide},invisible,start,stop';
