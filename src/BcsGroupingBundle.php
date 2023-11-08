0d4f0d462b5073b16c916669a040e48c97b46a75
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-11-08 12:38:06 -0500

    Create BcsGroupingBundle.php

---
+++ src/BcsGroupingBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @copyright  Bright Cliud Studio
+ * @author     Bright Cloud Studio
+ * @package    Contao CE Grouping
+ * @license    LGPL-3.0+
+ * @see	       https://github.com/bright-cloud-studio/contao-ce-grouping
+ */
+
+namespace Bcs\GroupingBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BcsGroupingBundle extends Bundle
+{
+}
